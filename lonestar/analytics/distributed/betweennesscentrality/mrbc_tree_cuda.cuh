e04c4032da0ddcb783495c357d9ac8a3c50f4e42
yhylord <uh@example.com>
2020-12-05 19:54:44 -0600

    Add basic CUDA MRBCTree

--- lonestar/analytics/distributed/betweennesscentrality/mrbc_tree_cuda.cuh
+++ lonestar/analytics/distributed/betweennesscentrality/mrbc_tree_cuda.cuh
@@ -3,10 +3,10 @@
 #include "galois/runtime/cuda/DeviceSync.h"
 #include "gpu_hash_table.cuh"
 
-struct MRBCTreeCUDA {
-  using BitSet = DynamicBitset;
-  using FlatMap = gpu_hash_table<uint32_t, BitSet, SlabHashTypeT::ConcurrentMap>;
+using BitSet = DynamicBitset;
+using FlatMap = gpu_hash_table<uint32_t, BitSet, SlabHashTypeT::ConcurrentMap>;
 
+struct MRBCTreeCUDA {
   FlatMap distanceTree;
 
   uint32_t numNonInfinity;
